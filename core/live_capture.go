41071a66ac38ea444b7f4fc787e90d629f2e650f
sangyun-han <sangyun628@gmail.com>
2019-07-23 15:41:17 +0900

    initial commit - add test code

---
+++ core/live_capture.go
@@ -0,0 +1,79 @@
+package core
+
+import (
+	"fmt"
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers"
+	"github.com/google/gopacket/pcap"
+	"log"
+	"time"
+)
+
+func Capture() {
+
+
+	var (
+		device       string = "en5"
+		snapshotLen int32  = 1024
+		promiscuous  bool   = false
+		err          error
+		timeout      time.Duration = 5 * time.Second
+		handle       *pcap.Handle
+	)
+
+
+
+	handle, err = pcap.OpenLive(device, snapshotLen, promiscuous, timeout)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer handle.Close()
+
+	count := 0
+	menu := 2
+
+
+	if menu == 1 {
+		packetSrc := gopacket.NewPacketSource(handle, handle.LinkType())
+		for packet := range packetSrc.Packets() {
+			count++
+			fmt.Println(count)
+			if packet.NetworkLayer() != nil {
+				fmt.Println(packet.NetworkLayer().NetworkFlow())
+			}
+
+		}
+
+	} else if menu == 2 {
+		var eth layers.Ethernet
+		var ip4 layers.IPv4
+		var ip6 layers.IPv6
+		var tcp layers.TCP
+
+
+		parser := gopacket.NewDecodingLayerParser(layers.LayerTypeEthernet, &eth, &ip4, &ip6, &tcp)
+		decoded := []gopacket.LayerType {}
+		for {
+			data, _, _ := handle.ZeroCopyReadPacketData()
+			parser.DecodeLayers(data, &decoded)
+
+			for _, layerType := range decoded {
+				switch layerType {
+
+				case layers.LayerTypeIPv6:
+					fmt.Println(ip6.SrcIP, ip6.DstIP)
+					count++
+					fmt.Println(count)
+				case layers.LayerTypeIPv4:
+					fmt.Println(ip4.SrcIP, ip4.DstIP)
+					count++
+					fmt.Println(count)
+				}
+			}
+		}
+	}
+
+
+
+
+}
---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"PacketCollector/core"
+	"log"
+	"net/http"
+	_ "net/http/pprof"
+	"time"
+)
+
+//var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to `file`")
+//var memprofile = flag.String("memprofile", "", "write mem profile to `file`")
+
+func main() {
+	//flag.Parse()
+	//if *cpuprofile != "" {
+	//	f, err := os.Create(*cpuprofile)
+	//	if err != nil {
+	//		log.Fatal("could not create CPU profile: ", err)
+	//	}
+	//	defer f.Close()
+	//	if err := pprof.StartCPUProfile(f); err != nil {
+	//		log.Fatal("could not start CPU profile: ", err)
+	//	}
+	//	defer pprof.StopCPUProfile()
+	//}
+	//
+	//if *memprofile != "" {
+	//	f, err := os.Create(*memprofile)
+	//	if err != nil {
+	//		log.Fatal("could not create memory profile: ", err)
+	//	}
+	//	defer f.Close()
+	//	runtime.GC() // get up-to-date statistics
+	//	if err := pprof.WriteHeapProfile(f); err != nil {
+	//		log.Fatal("could not write memory profile: ", err)
+	//	}
+	//}
+
+	go func() {
+		log.Println(http.ListenAndServe("localhost:6060", nil))
+	}()
+
+	go core.Capture()
+	time.Sleep(30 * time.Second)
+}
